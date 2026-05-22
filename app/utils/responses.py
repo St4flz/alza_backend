5da12220dc8e819e09a5aa8df34f724a6c913ad6
st4flz <st4flz@gmail.com>
2026-05-21 19:13:23 -0500

    Patch para conexion con front 2

--- app/utils/responses.py
+++ app/utils/responses.py
@@ -1,13 +1,14 @@
 from fastapi.responses import JSONResponse
+from fastapi.encoders import jsonable_encoder
 
 def success_response(data=None, message="OK", status_code=200):
     return JSONResponse(
         status_code=status_code,
-        content={
+        content=jsonable_encoder({
             "success": True,
             "message": message,
             "data": data
-        }
+        })
     )
 
 def created_response(data=None, message="Creado exitosamente"):
